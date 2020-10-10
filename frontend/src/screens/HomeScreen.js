fc53128282114a6e72487ca43424879dce0d3834
Eric Palmas <ericpalmas96@gmail.com>
2020-10-10 12:26:01 +0200

    basic frontend

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
+import products from '../products'
+
+const HomeScreen = () => {
+    return (
+        <>
+            <h1>Latest product</h1>
+            <Row>
+                {products.map(product => (
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product} />
+                        {/* <h3>{product.name}</h3> */}
+                    </Col>
+                ))}
+            </Row>
+
+        </>
+    )
+}
+
+export default HomeScreen
