480ef200e789a7fff6cd4a8aa9a4ecced579844e
Eric Palmas <ericpalmas96@gmail.com>
2020-10-27 23:17:10 +0100

    backend authentication

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 //i have to use .js when i import file (if i use modules)
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 
 dotenv.config()
@@ -14,11 +15,15 @@ connectDB();
 
 const app = express()
 
+// with this instruction i can pass json to the server
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound)
 app.use(errorHandler)
